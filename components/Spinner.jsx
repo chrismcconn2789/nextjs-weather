4f0d1a941821fc5f3588f75d4998ced4111ec99f
Chris <chrismc2789@gmail.com>
2022-08-02 10:20:16 +0100

    initial commit

---
+++ components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <>
+      <div className="text-4xl text-white text-center flex items-center h-screen justify-center text-[#59546C]">
+        Loading ...
+      </div>
+    </>
+  );
+};
+
+export default Spinner;
---
+++ components/Weather.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Weather = ({ data }) => {
+  console.log(data);
+  return (
+    <div className="flex flex-col max-w-[450px] w-full h-[90vh] m-auto p-4 text-gray-300">
+      <div className="flex justify-between pt-12">
+        <div className="flex flex-col items-center">
+          <Image
+            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+            alt="weather"
+            width="75"
+            height="75"
+          />
+          <p className="text-3xl">{data.weather[0].main}</p>
+        </div>
+        <div className="pt-[5%]">
+          <p className="text-7xl lg:text-8xl">
+            {data.main.temp.toFixed(0)} &#176;C
+          </p>
+        </div>
+      </div>
+
+      <div className="p-8 rounded-md bg-[#59546C]/20 mt-16">
+        <p className="pb-6 text-xl font-bold text-center lg:text-2xl">
+          Weather in {data.name}
+        </p>
+        <div className="flex justify-between text-center">
+          <div>
+            <p className="text-xl font-bold">
+              {data.main.feels_like.toFixed(0)} &#176;C
+            </p>
+            <p className="text-lg">Feels like</p>
+          </div>
+          <div>
+            <p className="text-xl font-bold">{data.main.humidity} %</p>
+            <p className="text-lg">Humidity</p>
+          </div>
+          <div>
+            <p className="text-xl font-bold">
+              {data.wind.speed.toFixed(0)} mph
+            </p>
+            <p className="text-lg">Winds</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
